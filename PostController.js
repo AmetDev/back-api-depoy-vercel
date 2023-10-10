59931a24610a5855a160cc58e58bca3d82fe91f7
AmetDev <ametsejdaliev@gmail.com>
2023-10-10 19:15:34 +0300

    database

--- PostController.js
+++ PostController.js
@@ -5,9 +5,9 @@ class ParkingController {
 	async create(req, res) {
 		try {
 			const parking = req.body
-			const parkingData = JSON.parse(fs.readFileSync('database.json'))
+			const parkingData = JSON.parse(fs.readFileSync('./database.json'))
 			parkingData.push(parking)
-			fs.writeFileSync('database.json', JSON.stringify(parkingData))
+			fs.writeFileSync('./database.json', JSON.stringify(parkingData))
 			res.json(parkingData)
 		} catch (e) {
 			res.status(500).json({ error: e.message })
@@ -16,7 +16,7 @@ class ParkingController {
 
 	async getAll(req, res) {
 		try {
-			const parkingData = JSON.parse(fs.readFileSync('database.json'))
+			const parkingData = JSON.parse(fs.readFileSync('./database.json'))
 			res.json(parkingData)
 		} catch (e) {
 			res.status(500).json({ error: e.message })
@@ -26,7 +26,7 @@ class ParkingController {
 	async getOne(req, res) {
 		try {
 			const id = req.params.id
-			const parkingData = JSON.parse(fs.readFileSync('database.json'))
+			const parkingData = JSON.parse(fs.readFileSync('./database.json'))
 			const parking = parkingData.find(p => p.owner_id === id)
 			res.json(parking)
 		} catch (e) {
@@ -37,12 +37,12 @@ class ParkingController {
 	async update(req, res) {
 		try {
 			const updatedParking = req.body
-			const parkingData = JSON.parse(fs.readFileSync('database.json'))
+			const parkingData = JSON.parse(fs.readFileSync('./database.json'))
 			const parkingIndex = parkingData.findIndex(
 				p => p.owner_id === updatedParking.owner_id
 			)
 			parkingData[parkingIndex] = updatedParking
-			fs.writeFileSync('database.json', JSON.stringify(parkingData))
+			fs.writeFileSync('./database.json', JSON.stringify(parkingData))
 			res.json(parkingData)
 		} catch (e) {
 			res.status(500).json({ error: e.message })
@@ -52,10 +52,10 @@ class ParkingController {
 	async delete(req, res) {
 		try {
 			const id = req.params.id
-			const parkingData = JSON.parse(fs.readFileSync('database.json'))
+			const parkingData = JSON.parse(fs.readFileSync('./database.json'))
 			const parkingIndex = parkingData.findIndex(p => p.owner_id === id)
 			const deletedParking = parkingData.splice(parkingIndex, 1)
-			fs.writeFileSync('database.json', JSON.stringify(parkingData))
+			fs.writeFileSync('./database.json', JSON.stringify(parkingData))
 			res.json(deletedParking)
 		} catch (e) {
 			res.status(500).json({ error: e.message })
